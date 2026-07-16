5228e2e0a2f800839d52632d3fba73f075d07719
Mayra <gungunjn08@gmail.com>
2026-07-17 02:43:14 +0530

    feat: Mongo_connection

--- main.go
+++ main.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func main() {
-	http.HandleFunc("/gunnu", gunnu_diva)
-	fmt.Println("on the port 8000")
-	http.ListenAndServe(":8000", nil)
+type Student struct {
+	Name   string
+	RollNo int32
 }
+
+func main() {
+	Port := ":8000"
+	var uri string
+	uri = "MONGODB_URI"
+	if uri == "" {
+		fmt.Println("uri is empty")
+	}
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	coll := client.Database("students_data").Collection("Students")
+	newStudent := Student{Name: "Gunnu", RollNo: 82}
+	result, err := coll.InsertOne(context.TODO(), newStudent)
+	if err != nil {
+		fmt.Println("error occured")
+	}
+	fmt.Println(result, result.InsertedID)
+	http.HandleFunc("/gunnu", gunnu_diva)
+	http.HandleFunc("/file", createFile)
+	fmt.Println("Server running on", Port)
+	http.ListenAndServe(Port, nil)
+
+}
+func createFile(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Create("gunnu.txt")
+	if err != nil {
+		fmt.Fprintln(w, "there is a error")
+	}
+	fmt.Fprintln(w, "file created successfully"+file.Name())
+}
+
 func gunnu_diva(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("gunnu is the diva")
+	fmt.Fprintln(w, "gunnu is the diva")
 }
